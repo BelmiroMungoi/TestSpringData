f692afc246b879b0f71c9451e6263f9af45a3ff8
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-08-23 14:14:06 +0200

    criando queries alternativas e condicionais

--- src/main/java/com/bbm/testspringdata/dao/UsuarioRepository.java
+++ src/main/java/com/bbm/testspringdata/dao/UsuarioRepository.java
@@ -1,11 +1,33 @@
 package com.bbm.testspringdata.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.bbm.testspringdata.model.Usuario;
 
 @Repository
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
 
+	@Query(value = "select p from Usuario p where p.username like %?1%")
+	public List<Usuario> findByName(String name);
+	
+	@Query(value = "select p from Usuario p where p.username =:paramNome")
+	public Usuario findByNameParam(@Param("paramNome") String paramNome);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "delete from Usuario u where u.username = ?1")
+	public void deleteByName(String name);
+	
+	@Modifying
+	@Transactional
+	@Query(value = "update Usuario u set u.email = ?1 where username = ?2")
+	public void updateByName(String email, String name);
+	
 }
