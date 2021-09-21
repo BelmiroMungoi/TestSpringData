c9d2bf3c77c98f2c7acf38e26180b7d78db1bb10
Belmiro Mungoi <belmiromungoi@gmail.com>
2021-09-21 15:19:06 +0200

    entidade projecto

---
+++ src/main/java/com/bbm/testspringdata/model/Projecto.java
@@ -0,0 +1,51 @@
+package com.bbm.testspringdata.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Projecto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String nome;
+	
+	@ManyToMany(mappedBy = "projectos", cascade = CascadeType.ALL)
+	private List<Usuario> usuarios;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+}
